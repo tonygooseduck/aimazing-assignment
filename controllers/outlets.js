fcf3f2d2aeef64363b73ba7c20418407b26e844b
Iu Chian Tang <tonygooseduck@gmail.com>
2019-07-02 19:35:05 +0800

    Finish outletsrouter api
    - Create outlets
    - Get all outlets for an user
    - Get single outlet information
    - Update single outlet information for an user
    - Delete single outlet for an user

--- controllers/outlets.js
+++ controllers/outlets.js
@@ -34,19 +34,41 @@ outletsRouter.post('/', async (req, res, next) => {
         }
         const userId = await getUserId(accessToken);
         const status = await insert(body, userId);
-        res.sendStatus(200);
+        res.sendStatus(status);
     } catch (exception) {
         next(exception);
     }
 });
 
-outletsRouter.delete('/:id', (req, res, next) => {
+outletsRouter.delete('/:id', async (req, res, next) => {
     const outletId = req.params.id;
+    const accessToken = getTokenFrom(req);
+    try {
+        if (!accessToken) {
+            res.status(401).send({ error: 'token missing' });
+        }
+        const userId = await getUserId(accessToken);
+        const status = await deleteOutlet(userId, outletId);
+        res.sendStatus(204);
+    } catch (exception) {
+        next(exception);
+    }
 });
 
-outletsRouter.put('/:id', (req, res, next) => {
+outletsRouter.put('/:id', async (req, res, next) => {
     const outletId = req.params.id;
     const body = req.body;
+    const accessToken = getTokenFrom(req);
+    try {
+        if (!accessToken) {
+            res.status(401).send({ error: 'token missing' });
+        }
+        const userId = await getUserId(accessToken);
+        const newOutlet = await updateOutlet(body, userId, outletId);
+        res.json(newOutlet);
+    } catch (exception) {
+        next(exception);
+    }
 });
 
 function getTokenFrom(request) {
@@ -111,9 +133,38 @@ function insert(body, userId) {
             if (error) {
                 reject(error);
             } else {
-                resolve(200);
+                resolve(201);
             }
         });
     })
+}
+
+function deleteOutlet(userId, outletId) {
+    return new Promise((resolve, reject) => {
+        db.query('delete from outlet where user_id = ? and id = ?', [userId, outletId], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(204);
+            }
+        });
+    });
+}
+
+function updateOutlet(body, userId, outletId) {
+    return new Promise((resolve, reject) => {
+        let outlet = {
+            name: body.name,
+            phone: body.phone,
+            address: body.address
+        };
+        db.query('update outlet set ? where user_id = ? and id = ?', [outlet, userId, outletId], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
 }
 module.exports = outletsRouter;
